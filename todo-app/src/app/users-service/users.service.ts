e4c262f59a3f7010f53ea819b7b6662d586203d0
Nisha Bharti <nishabharti@Nishas-MacBook-Pro.local>
2020-03-07 16:09:57 -0700

    added strikethrough feature

---
+++ todo-app/src/app/users-service/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  usersUrl= 'assets/users.json'
+
+  getUsers(){
+    return this.http.get(this.usersUrl)
+  }
+}
